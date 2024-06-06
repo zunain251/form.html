b6203ab9e04943c94df9aded30891d80dbafa987
Mohammed Zunain.BZ <zunainbz251@gmail.com>
2024-06-06 10:16:11 +0530

    Add files via upload

---
+++ getdata.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Dta from HTML page</title>
+</head>
+<body>
+    <?php
+    echo "Name:".$_POST["cname"]."</br>";
+    echo "Email:".$_POST["cemail"]."</br>";
+    echo "Message:".$_POST["cmessage"]."</br>";
+    ?>
+    </body>
+    </html>
